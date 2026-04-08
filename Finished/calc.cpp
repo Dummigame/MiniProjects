029316cee1c8934b427c2f807b38948fb913d1f8
Dummigame <Dummigame@duck.com>
2026-04-09 00:42:14 +0200

    Using .at() method instead of direct index and more fixes

--- Finished/calc.cpp
+++ Finished/calc.cpp
@@ -25,6 +25,7 @@ int main()
     }
     bool broken_equation{};
     broken_equation=sanitize_equation(equation);
+    broken_equation:
     if(broken_equation)
     {
         std::cout << "\n==============================================\nYour equation could not be sanitized! Exiting.\n==============================================\n";
@@ -32,6 +33,13 @@ int main()
     }
     std::string operators {get_operators(equation)};
     std::vector<long double> numbers{get_numbers(equation)};
+
+    if(numbers.size()<= operators.size())
+    {
+        broken_equation=true;
+        goto broken_equation;
+    }
+
     if(numbers.size()<2)
     {
         std::cout << equation << std::endl;
@@ -61,8 +69,8 @@ std::string get_operators(const std::string &input)
     std::string operators;
     for(long unsigned int i=0; i<input.length(); i++)
     {
-        if(input[i] == '+' || input[i] == '*' || input[i] == '/' || input[i] == '^') operators.push_back(input[i]);
-        if(input[i]=='-' && i!=0 && input[i-1]!= '^' && input[i-1]!= '*' && input[i-1]!= '/' && input[i-1]!= '+' ) operators.push_back('+');
+        if(input.at(i) == '+' || input.at(i) == '*' || input.at(i) == '/' || input.at(i) == '^') operators.push_back(input.at(i));
+        if(input.at(i)=='-' && i!=0 && input.at(i-1)!= '^' && input.at(i-1)!= '*' && input.at(i-1)!= '/' && input.at(i-1)!= '+' ) operators.push_back('+');
     }
     return operators;
 }
@@ -89,8 +97,8 @@ std::vector<long double> get_numbers(const std::string &input_string)
 
             if((input_string[i] >='0' && input_string[i]<='9') || input_string[i] == '.')
             {
-                if(i>0) if(input_string[i-1]=='-') current_number.push_back('-');
-                current_number.push_back(input_string[i]);
+                if(i>0) if(input_string.at(i-1)=='-') current_number.push_back('-');
+                current_number.push_back(input_string.at(i));
                 number_present=true;
             }
             else
@@ -120,62 +128,63 @@ long double calculation(std::string &operators, std::vector<long double> &number
         {
             if(i_==0 && i==0)
             {
-                for(int i__=operators.length(); i__>=0; i__--)
+                for(int i__=operators.length()-1; i__>=0; i__--)
                 {
-                    if(operators[i__]=='^')
-                    {
-                        result_of_previous=evaluate_two_numbers(numbers[i__], numbers[i__+1], operators[i__]);
+                    if(operators.at(i__)=='^')
+                    {                    
+                        result_of_previous=evaluate_two_numbers(numbers.at(i__), numbers.at(i__+1), operators.at(i__));
 
                         numbers[i__+1]=result_of_previous;
-                        if(numbers[i__]<0 && numbers[i__+1]>0)
+                        if(numbers.at(i__)<0 && numbers.at(i__+1)>0)
                         {
-                            numbers[i__+1]=(result_of_previous*(-1));
+                            numbers.at(i__+1)=(result_of_previous*(-1));
                             result_of_previous=result_of_previous*(-1);
                         }
-
+                        
                         numbers.erase(numbers.begin()+i__);
                         operators.erase(operators.begin()+i__);
-                        i__++;
                         continue;
                     }
                 }
             }
-            if((operators[i]=='/' || operators[i]=='*') && i_==1)
+            if(!operators.length()) continue;
+            if((operators.at(i)=='/' || operators.at(i)=='*') && i_==1)
             {
                 //Replaces calculated two-number equation with its result and removes the used operator and numbers
-                result_of_previous=evaluate_two_numbers(numbers[i], numbers[i+1], operators[i]);
+                result_of_previous=evaluate_two_numbers(numbers.at(i), numbers.at(i+1), operators.at(i));
 
-                if(numbers[i+1]<0) operators.erase(operators.begin()+i);
-
-                numbers[i]=result_of_previous;
+                numbers.at(i)=result_of_previous;
                 numbers.erase(numbers.begin()+i+1);
                 operators.erase(operators.begin()+i);
                 i--;
                 continue;
                 //I feel smart now
             }
-            else if(operators[i]=='+' && i_==2)
+            else if(operators.at(i)=='+' && i_==2)
             {
-                if(i==0) result_of_previous=evaluate_two_numbers(numbers[0], numbers[1], operators[i]);
-                else result_of_previous = evaluate_two_numbers(result_of_previous, numbers[i+1], operators[i]);
+                if(i==0) result_of_previous=evaluate_two_numbers(numbers.at(0), numbers.at(1), operators.at(i));
+                else result_of_previous = evaluate_two_numbers(result_of_previous, numbers.at(i+1), operators.at(i));
             }
         }
     }
     return result_of_previous;
-}
+} 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-bool sanitize_equation(std::string &equation)
+bool sanitize_equation(std::string &equation) 
 {
     bool is_unsavable{};
     bool sanitization_required{};
+
+    int digits{};
+    int symbols{};
 
 
     //Removes garbage and operators before start of equation
     for(int i__{}; i__<=equation.length(); i__++)
     {
-        if(!(equation[i__]>='0' && equation[i__]<='9') && equation[i__]!='-')
+        if(!(equation.at(i__)>='0' && equation.at(i__)<='9') && equation.at(i__)!='-')
         {
             equation.erase(equation.begin());
             if(!equation.length())
@@ -184,6 +193,7 @@ bool sanitize_equation(std::string &equation)
                 break;
             }
             i__--;
+            sanitization_required=true;
         }
         else break;
     }
@@ -191,44 +201,64 @@ bool sanitize_equation(std::string &equation)
     //Removes garbage in equation
     for(long unsigned int i_{}; i_<equation.length(); i_++)
     {
-        if(!(equation[i_]>='0' && equation[i_]<='9') && equation[i_]!='-' && equation[i_]!='+' && equation[i_]!= '*' && equation[i_]!= '/' && equation[i_]!='.' && equation[i_]!='^')
+        if(!(equation.at(i_)>='0' && equation.at(i_)<='9') && equation.at(i_)!='-' && equation.at(i_)!='+' && equation.at(i_)!= '*' && equation.at(i_)!= '/' && equation.at(i_)!='.' && equation.at(i_)!='^')
         {
             equation.erase(equation.begin()+i_);
             i_--; //Yes this causes an underflow but it doesn't matter since it is the last time i_ is used before being incremented again
             sanitization_required=true;
         }
         long unsigned int final_element{equation.length()-1};
-        if(equation[final_element] == '+' || equation[final_element] == '*' || equation[final_element] == '/' || equation[final_element] == '^') equation.erase(--equation.end());
+        if(equation.at(final_element) == '+' || equation.at(final_element) == '*' || equation.at(final_element) == '/' || equation.at(final_element) == '^') equation.erase(--equation.end());
     }
 
     for(long unsigned int i{}; i<equation.length(); i++)
     {
         if(i+1<equation.length())
         {
-            if(equation[i]=='-' && equation[i+1]=='-')
+            if(equation.at(i)=='-' && equation.at(i+1)=='-')
             {
-                equation[i]='+';
+                equation.at(i)='+';
                 equation.erase(equation.begin()+i+1);
-                if(i==0) equation.erase(equation.begin());
-                sanitization_required=true;
-                if(i>1) if((equation[i-1]=='*' || equation[i-1]=='/' || equation[i-1]=='+' || equation[i-1]=='^')) equation.erase(equation.begin()+i);
+                if(i==0)
+                {
+                    equation.erase(equation.begin());
+                    i--;
+                    sanitization_required=true;
+                    continue;
+                }
+                
+                if(i>1) if((equation.at(i-1)=='*' || equation.at(i-1)=='/' || equation.at(i-1)=='+' || equation.at(i-1)=='^'))
+                {
+                    equation.erase(equation.begin()+i);
+                    i--;
+                    sanitization_required=true;
+                    continue;
+                }
             }
             if((equation[i]=='+' && equation[i+1]=='+') || (equation[i]=='+' && equation[i+1]=='-'))
             {
                 equation.erase(equation.begin()+i);
+                sanitization_required=true;
             }
             if(equation[i]=='-' && equation[i+1]=='+') is_unsavable=true;
 
-            if(((equation[i]=='*' || equation[i]=='/' || equation[i]=='+' || equation[i]=='^' || equation[i]=='-') && (equation[i+1]=='*' || equation[i+1]=='/' || equation[i+1]=='+' || equation[i+1]=='^')))
+            if(((equation.at(i)=='*' || equation.at(i)=='/' || equation.at(i)=='+' || equation.at(i)=='^' || equation.at(i)=='-') && (equation.at(i+1)=='*' || equation.at(i+1)=='/' || equation.at(i+1)=='+' || equation.at(i+1)=='^')))
             {
                 is_unsavable=true;
             }
-            if((equation[i]=='-' && (equation[i+1]=='*' || equation[i+1]=='/')))
+            if((equation.at(i)=='-' && (equation.at(i+1)=='*' || equation.at(i+1)=='/')))
             {
                 is_unsavable=true;
             }
         }
     }
+
+    for(unsigned long i___{}; i___<equation.length(); i___++)
+    {
+        if((equation.at(i___)>='0' && equation.at(i___)<='9') || equation.at(i___)=='-') digits++;
+        else symbols++;
+    }
+    if(symbols >= digits) is_unsavable=true;
     if(equation.length()<3) return true; // <3
     if(sanitization_required && !is_unsavable) std::cout << "\n=================================================================\n|| Your equation had to be sanitized. Output may be incorrect! ||\n=================================================================\nNew equation: " << equation << "\n";
     return is_unsavable;
